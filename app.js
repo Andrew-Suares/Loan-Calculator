859d7ff312030c856c203b18c3da427cc13ed0a2
Andrew Suares <andrewlsuares@gmail.com>
2021-06-08 08:06:45 -0700

    Calc and Error
    Added Calcs and error message

--- app.js
+++ app.js
@@ -1,21 +1,63 @@
-// Listen for submits step 1
+// Listen for submit
 document
-	.querySelector('#loan-form')
+	.getElementById('loan-form')
 	.addEventListener('submit', calculateResults)
 
-// Calculate results Function step 2
-
+// Calculate Results
 function calculateResults(e) {
-	// UI Variables Step 3
-	const amount = document.querySelector('#amount')
-	const interest = document.querySelector('#interest')
-	const years = document.querySelector('#years')
-	const monthlyPayment = document.querySelector('#monthly-payment')
-	const totalPayment = document.querySelector('#total-payment')
-	const totalInterest = document.querySelector('#total-interest')
-	// Calculations Step 4
-	// ! parse float changes it to a decimal
+	console.log('Calculating...')
+	// UI Vars
+	const amount = document.getElementById('amount')
+	const interest = document.getElementById('interest')
+	const years = document.getElementById('years')
+	const monthlyPayment = document.getElementById('monthly-payment')
+	const totalPayment = document.getElementById('total-payment')
+	const totalInterest = document.getElementById('total-interest')
+	// Parse Float changes it to a decimal
 	const principal = parseFloat(amount.value)
 	const calculatedInterest = parseFloat(interest.value) / 100 / 12
+	const calculatedPayments = parseFloat(years.value) * 12
+
+	// Compute monthly payment
+	const x = Math.pow(1 + calculatedInterest, calculatedPayments)
+	const monthly = (principal * x * calculatedInterest) / (x - 1)
+
+	// The to fixed changes it to two decimal points
+	if (isFinite(monthly)) {
+		// Display the data in the results field
+		monthlyPayment.value = monthly.toFixed(2)
+		totalPayment.value = (monthly * calculatedPayments).toFixed(2)
+		totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2)
+	} else {
+		showError('Please check your numbers')
+	}
+
 	e.preventDefault()
+}
+
+// Show Error
+function showError(error) {
+	// Create a div
+	const errorDiv = document.createElement('div')
+
+	// Get elements
+	const card = document.querySelector('.card')
+	const heading = document.querySelector('.heading')
+
+	// Add class
+	errorDiv.className = 'alert alert-danger'
+
+	// Create text node and append to div
+	errorDiv.appendChild(document.createTextNode(error))
+
+	// Insert error above heading
+	card.insertBefore(errorDiv, heading)
+
+	// Clear error after 3 seconds
+	setTimeout(clearError, 3000)
+}
+
+// Clear error
+function clearError() {
+	document.querySelector('.alert').remove()
 }
